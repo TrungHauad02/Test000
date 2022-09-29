37222dcbb36c88341af78619a479b50f9def64fd
MinhTien21133088 <114594876+MinhTien21133088@users.noreply.github.com>
2022-09-29 17:09:38 +0700

    Update Program.cs

--- Test000/Program.cs
+++ Test000/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test000
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
