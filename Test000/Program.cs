33f736a39f67945744a0727bfe04403f4d96129b
MinhTien21133088 <21133088@student.hcmute.edu.vn>
2022-09-29 17:22:49 +0700

    Bo conflict

--- Test000/Program.cs
+++ Test000/Program.cs
@@ -11,6 +11,7 @@ namespace Test000
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
         }
     }
 }
